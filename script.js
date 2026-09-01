9bb72687a04ef00ba0d39416133130afc25fe758
sofiamskora-cyber <sofiamskora@gmail.com>
2026-09-01 09:02:55 -0300

    ok

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+if(likeBtn) return;
+    const likesCountSpan = likeBtn.querySelector("likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
